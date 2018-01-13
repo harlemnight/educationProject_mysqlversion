b06aff746bf08a59c66ce83a2c19f37965b77578
harlemnight <harlemnight.jeff@gmail.com>
2018-01-13 13:58:05 +0800

    add

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,71 @@
 var express = require('express');
+var mysql = require('mysql');
+var dbConfig = require('../sqlmap/mysqldb');
+var BabySql = require('../sqlmap/babysql');
+var OrderSql = require('../sqlmap/ordersql');
 var router = express.Router();
+var pool = mysql.createPool( dbConfig.mysql );
+
 router.get('/', function(req, res, next) {res.render('index', {});});
+
+/*
+* 办卡续卡列表
+* */
+router.get('/list', function(req, res) {
+    var baby_name = req.query.baby_name;
+    var order_rqq = req.query.course_rqq;
+    var order_rqz = req.query.course_rqz;
+    var num = req.query.page;
+    var pageNum = 0;
+    var pageSize = 2;
+    if ( num == undefined || num <= 1) {
+        pageNum = 1;
+    }else {
+        pageNum = num;
+    }
+    var offset = (parseInt(pageNum)-1)*pageSize;
+    var param ;
+    var querySqlResult ;
+    var querySqlCount ;
+
+
+    pool.getConnection(function(err, connection) {
+        connection.query(querySqlResult, param, function(err, orders) {
+            if(err){
+                res.render('order/list', {
+                    status:false,
+                    msg:err.toString(),
+                    active_url:'order/list'
+                });
+            }else {
+                connection.query(querySqlCount,param,function(err, result) {
+                    if(err){
+                        res.render('order/list', {
+                            status:false,
+                            msg:err.toString(),
+                            active_url:'order/list'
+                        });
+                    }else {
+                        var pageTotal =Math.ceil(result[0].cnt/pageSize);
+                        res.render('order/list', {
+                            status:true,
+                            orders:orders,
+                            order_rqq:order_rqq,
+                            order_rqz:order_rqz,
+                            baby_name:baby_name,
+                            pageNum:pageNum,
+                            pageTotal:pageTotal,
+                            active_url:'order/list',
+                            offset:offset
+                        });
+                    }
+                    connection.release();
+                });
+            }
+        });
+    });
+});
+
+
+
 module.exports = router;
--- sqlmap/ordersql.js
+++ sqlmap/ordersql.js
@@ -1,54 +1,22 @@
 var OrderSql = {
-    insert :    'insert into babies ' +
+    insert :    'insert into orders ' +
                 '(' +
-                'baby_name,' +
-                'birthday,' +
-                'age,' +
-                'father,' +
-                'mather,' +
-                'grandpa,' +
-                'grandma,' +
-                'home_address,' +
-                'phone_no1,' +
-                'phone_no2,' +
-                '`case`,' +
-                'allergy,' +
-                'hobby,' +
-                '`character`,' +
+                'baby_id,' +
+                'order_date,' +
                 'member_lx,' +
-                'init_count,' +
-                'course_count'+
+                'course_count,' +
+                'czlx_dm,' +
+                'je' +
                 ') ' +
-                'values(?,str_to_date(?, \'%Y-%m-%d\'),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-    queryAll :  'select * from babies where yxbz ="Y" order by lrrq desc limit ?,?',
-    queryAllBySearch :  'select * from babies where yxbz ="Y" and ' +
+                'values(?,str_to_date(?, \'%Y-%m-%d\'),?,?,?,?)',
+    queryAll :  'select * from orders where yxbz ="Y" order by lrrq desc limit ?,?',
+    queryAllBySearch :  'select * from orders where yxbz ="Y" and ' +
                         '( baby_name= ? or phone_no1 = ? or father = ? or mather = ?) ' +
                         'order by lrrq desc limit ?,?',
-    queryCount :   'select count(1) cnt from babies where yxbz ="Y"',
-    queryCountBySearch :   'select count(1) cnt from babies where yxbz ="Y" and ' +
+    queryCount :   'select count(1) cnt from orders where yxbz ="Y"',
+    queryCountBySearch :   'select count(1) cnt from orders where yxbz ="Y" and ' +
                         '( baby_name= ? or phone_no1 = ? or father = ? or mather = ?) ',
-    getBabyById :   'select * from babies where _id = ? ',
-    updateBaby  :   'update babies set ' +
-                    'baby_name = ?,' +
-                    'birthday = str_to_date(?, \'%Y-%m-%d\') ,' +
-                    'age = ?, ' +
-                    'father = ?, ' +
-                    'mather = ?, ' +
-                    'grandpa = ?, ' +
-                    'grandma = ?,' +
-                    'home_address = ? ,' +
-                    'phone_no1 = ? ,' +
-                    'phone_no2 = ? ,' +
-                    '`case` = ? ,' +
-                    'allergy = ? ,' +
-                    'hobby = ? ,' +
-                    '`character` = ? ,' +
-                    'member_lx = ? ,' +
-                    'init_count = ? ,' +
-                    'course_count = ? ,' +
-                    'xgrq = now() '+
-                    'where _id = ? ',
-    deleteBabyById :   'update babies set yxbz = "N" ,xgrq = now() where _id = ?'
+    getOrderById :   'select * from orders where _id = ? ',
 };
 
 module.exports = OrderSql;
